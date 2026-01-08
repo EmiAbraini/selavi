07b441afa6cdf0bf8c54af2799af5668af0a1693
EmanuelaAbraini <e.abraini@sinossi.it>
2026-01-08 14:31:51 +0100

    Delete files

--- js/partner.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const menuCircles = document.querySelectorAll('.menu-circle');
-
-  menuCircles.forEach(menuCircle => {
-      menuCircle.addEventListener('click', function() {
-          window.location.href = 'menu.html';
-      });
-  });
-});
